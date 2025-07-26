f055c023d450743fa8efd2d2dbebf91488820a02
youni20 <moyounus20@gmail.com>
2025-07-26 22:48:48 +0200

    Got the app to work

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
+import json
+import random
 
 app = Flask(__name__)
 
+last_index = None
+quotes = []
+author = []
+with open("quotes.json", "r") as file:
+    data = json.load(file)
+    for item in data:
+        quotes.append(item["quote"])
+        author.append(item["author"])
+
+
 @app.route("/")
-def home() -> render_template:
-    return(render_template("index.html")) # No need to specify folder as by default it looks inside folder named templates. 
+def home():
+    quote, auth = getQuote()
+    return(render_template("index.html", quote=quote, auth=auth)) # No need to specify folder as by default it looks inside folder named templates. 
+
+@app.route("/api/quote")
+def api_quote():
+    quote, auth = getQuote()
+    return jsonify({"quote": quote, "auth": auth})
+
+
+def getQuote():
+    global last_index
+    if len(quotes) == 0:
+        return "No quotes available.", "Unknown"
+    
+    if len(quotes) == 1:
+        num = 0
+    else:
+        num = random.randint(0, len(quotes)-1)
+        while num == last_index:
+            num = random.randint(0, len(quotes)-1)
+
+    last_index = num
+    return quotes[num], author[num]
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000) # So if any errors it shows on webpage.
+    app.run(debug=True, port=8000) # Debug=True, so if any errors it shows on webpage.
+
+
+#15:30

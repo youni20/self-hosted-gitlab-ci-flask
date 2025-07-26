d2c795be9e523f099fca7fcd974532164817b86e
youni20 <moyounus20@gmail.com>
2025-07-26 21:27:11 +0200

    Flask app with HTML and CSS implemented

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def home() -> str:
-    return("Hello")
+def home() -> render_template:
+    return(render_template("index.html")) # No need to specify folder as by default it looks inside folder named templates. 
 
 if __name__ == "__main__":
-    app.run(debug=True) # So if any errors it shows on webpage.
+    app.run(debug=True, port=8000) # So if any errors it shows on webpage.
